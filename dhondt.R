fd422782ece22eb5144f21836e7894164465cf31
picanum <10380748+picanum@users.noreply.github.com>
2019-05-27 12:37:33 +0200

    Create dhondt.R

---
+++ dhondt.R
@@ -0,0 +1,18 @@
+dhondt <- function(x, nombres = NULL, escanos, umbral){
+    x <- x[which(x/sum(x) >= umbral)]
+    if(!is.null(nombres)) nombres <- nombres[which(x/sum(x) >= umbral)]
+    temp1 <- sort(sapply(1:escanos, function(i) x/i), decreasing=T)[1:escanos]
+    mat <- matrix(sapply(1:escanos, function(i) x/i), ncol = length(x), byrow=T)
+    if(is.null(nombres)){
+        colnames(mat) <- names(x)
+    }
+    else{
+        colnames(mat) <- nombres
+    }
+    Escaños <- NULL
+    for(i in 1:escanos){
+        Escaños <- c(Escaños, colnames(mat)[which(mat[i,] >= min(temp1))])
+    }
+    ret <- sort(table(Escaños),decreasing=T)
+    return(ret)
+}
